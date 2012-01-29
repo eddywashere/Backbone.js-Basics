b3fdcc94b8fda1689dc6b03a074a527bb02fcb56
eddy <therealeddy@gmail.com>
2012-01-28 21:16:38 -0600

    Part 1 completed

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -24,6 +24,8 @@
 		url: '/albums'	
 	});
 	
+	window.library = new Albums();
+	
 	window.AlbumView = Backbone.View.extend({
 		
 		tagName: 'li',
@@ -41,16 +43,63 @@
 		}
 	});
 	
-	window.LibraryAlbumView = AlbumView({
+	window.LibraryAlbumView = AlbumView.extend({
 		
 	});
 	
 	window.LibraryView = Backbone.View.extend({
+		tagName: 'section',
+		className: 'library',
+		
 		initialize: function() {
 			_.bindAll(this, 'render');
 			this.template = _.template($('#library-template').html());
+			this.collection.bind('reset', this.render);
+		},
+		render: function() {
+			var $albums,
+				collection = this.collection;
+			
+			$(this.el).html(this.template({}));
+			$albums = this.$('.albums');
+			this.collection.each(function(album) {
+				var view = new LibraryAlbumView({
+					model: album,
+					collection: collection
+				});
+				$albums.append(view.render().el);
+			});
+			
+			return this;
 		}
 	});
 	
+	window.BackboneTunes = Backbone.Router.extend({
+		routes: {
+			'' : 'home',
+			'blank' : 'blank'
+		},
+		
+		initialize: function() {
+			this.libraryView = new LibraryView({
+				collection: window.library
+			});
+		},
+		
+		home: function() {
+			var $container = $('#container');
+			$container.empty();
+			$container.append(this.libraryView.render().el);
+		},
+		
+		blank: function() {
+			$('#container').empty().text('blank');
+		}
+	});
+	
+	$(function() {
+		window.App = new BackboneTunes();
+		Backbone.history.start();
+	});
 
 })(jQuery);
